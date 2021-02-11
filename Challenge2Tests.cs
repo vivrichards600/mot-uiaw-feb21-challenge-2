09fa1dbaf04e2cfa8486d84ae463e8c3ab3f70f7
Viv Richards <1014468+vivrichards600@users.noreply.github.com>
2021-02-11 21:47:04 +0000

    Not happy with sleep but reduced for now by 4500 ms

--- Challenge2Tests.cs
+++ Challenge2Tests.cs
@@ -30,7 +30,7 @@ namespace Challenge_2
             driver.FindElement(By.Id("password")).SendKeys("password");
             driver.FindElement(By.Id("doLogin")).Click();
 
-            Thread.Sleep(5000);
+            Thread.Sleep(500);
 
             IWebElement webElement = driver.FindElement(By.ClassName("navbar-collapse"));
             Console.WriteLine(webElement.Text);
